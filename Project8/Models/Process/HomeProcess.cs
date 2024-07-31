697ab46fd30929e73c12613838c48683df3623d3
LeNguyenThienDinh <hellow12343@gmail.com>
2024-07-31 19:28:33 +0700

    commit book website to git

---
+++ Project8/Models/Process/HomeProcess.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebBanSach.Models.Data;
+
+namespace WebBanSach.Models.Process
+{
+    public class HomeProcess
+    {
+        BSDBContext db = null;
+
+        public HomeProcess()
+        {
+            db = new BSDBContext();
+        }
+
+        public List<TheLoai> ListCategory()
+        {
+            return db.TheLoais.OrderBy(x => x.MaLoai).ToList();
+        }
+
+        public int InsertContact(LienHe entity)
+        {
+            db.LienHes.Add(entity);
+            db.SaveChanges();
+
+            return entity.MaLH;
+        }
+
+        public List<Sach> Search(string key)
+        {
+            return db.Saches.Where(x => x.TenSach.Contains(key)).OrderBy(x=>x.TenSach).ToList();
+        }
+
+    }
+}
---
+++ Project8/Models/Process/OrderProcess.cs
@@ -0,0 +1,37 @@
+﻿using WebBanSach.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebBanSach.Models.Process
+{
+    public class OrderProcess
+    {
+        //khởi tạo dữ liệu từ tầng data
+        BSDBContext db = null;
+
+        //contructor
+        public OrderProcess()
+        {
+            db = new BSDBContext();
+        }
+
+        public DonDatHang GetIdOrder(int id)
+        {
+            return db.DonDatHangs.Find(id);
+        }
+
+        public List<DonDatHang> ListOrder()
+        {
+            return db.DonDatHangs.OrderBy(x => x.MaDDH).ToList();
+        }
+
+        public int Insert(DonDatHang order)
+        {
+            db.DonDatHangs.Add(order);
+            db.SaveChanges();
+            return order.MaDDH;
+        }
+    }
+}
